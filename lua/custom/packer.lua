11410ddc28bd08eeb671d357d13e751d994f19b3
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2023-07-20 10:33:44 -0700

    Adding prettier and git-conflicts

--- lua/custom/packer.lua
+++ lua/custom/packer.lua
@@ -72,4 +72,16 @@ return require('packer').startup(function(use)
         -- tag = "*"
     }
 
+    use { 'akinsho/git-conflict.nvim', tag = "v1.0.0", config = function()
+        require('git-conflict').setup({
+            default_mappings = true, -- disable buffer local mapping created by this plugin
+            default_commands = true, -- disable commands created by this plugin
+            disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+            highlights = {
+                               -- They must have background color, otherwise the default color will be used
+                incoming = 'DiffText',
+                current = 'DiffAdd',
+            }
+        })
+    end }
 end)
