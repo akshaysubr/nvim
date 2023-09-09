d01b43075ddda00fe81acb7d237749bca291b564
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2023-09-08 17:27:12 -0700

    Adding nightfox config

---
+++ after/plugin/nightfox.lua
@@ -0,0 +1,13 @@
+local ok, nightfox = pcall(require, 'nightfox')
+if ok then
+    nightfox.setup({
+        options = {
+            -- Compiled file's destination location
+            transparent = true,            -- Disable setting background
+            dim_inactive = true,           -- Non focused panes set to alternative background
+        },
+    })
+
+    -- setup must be called before loading
+    vim.cmd("colorscheme nightfox")
+end
