6df62bbbbb014481b63001771e189200b4eb8339
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2023-10-06 10:04:39 -0700

    Moving lsp-zero to v3

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,12 @@ local ok, lsp = pcall(require, 'lsp-zero')
 if ok then
     lsp.preset('recommended')
 
-    lsp.ensure_installed({
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+        ensure_installed = {},
+        handlers = {
+            lsp.default_setup,
+        }
     })
 
     local cmp = require('cmp')
@@ -22,6 +27,28 @@ if ok then
             completeopt = 'menu,menuone,noinsert,noselect'
         },
     })
+
+    -- local cmp = require('cmp')
+    -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    -- cmp.setup({
+    --     sources = {
+    --         { name = 'path' },
+    --         { name = 'nvim_lsp' },
+    --         { name = 'nvim_lua' },
+    --     },
+    --     formatting = lsp.cmp_format(),
+    --     mapping = cmp.mapping.preset.insert({
+    --         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    --         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    --         ['<cr>'] = cmp.mapping.confirm({ select = true }),
+    --         ['<C-Space>'] = cmp.mapping.complete(),
+    --     }),
+    --     preselect = 'none',
+    --     completion = {
+    --         completeopt = 'menu,menuone,noinsert,noselect'
+    --     },
+    -- })
 
     lsp.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
