813c182dae9c674c8705222dfe0c2ac069ee19c1
Akshay Subramaniam <6964110+akshaysubr@users.noreply.github.com>
2024-01-07 16:20:09 -0800

    Adding some keymaps for splits and vim-maximizer plugin

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,13 +1,13 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move visual selection down"})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move visual selection up"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move visual selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move visual selection up" })
 
-vim.keymap.set("n", "J", "mzJ`z", {desc = "Bring line from below up into this line"})
-vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Fast scroll down keeping curson in the center"})
-vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Fast scroll up keeping curson in the center"})
-vim.keymap.set("n", "n", "nzzzv", {desc = "Go to next selection keeping curson in the center"})
-vim.keymap.set("n", "N", "Nzzzv", {desc = "Go to prev selection keeping curson in the center"})
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Bring line from below up into this line" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Fast scroll down keeping curson in the center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Fast scroll up keeping curson in the center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next selection keeping curson in the center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to prev selection keeping curson in the center" })
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
@@ -20,18 +20,33 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {desc = "Format file"})
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format file" })
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Split window
+vim.keymap.set("n", "<leader>ss", "<C-w>s", { silent = true, noremap = true, desc = { "Horizontal split" } })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { silent = true, noremap = true, desc = { "Vertical split" } })
+vim.keymap.set("n", "<leader>sr", "<C-w>r", { silent = true, noremap = true, desc = { "Swap splits" } })
 
-local n_opts = {silent = true, noremap = true, desc = {"Better window navigation"}}
-local t_opts = {silent = true, desc = {"Terminal mode window navigation"}}
+vim.keymap.set("n", "<leader>sH", "<C-w>H", { silent = true, noremap = true, desc = { "Move split left" } })
+vim.keymap.set("n", "<leader>sJ", "<C-w>J", { silent = true, noremap = true, desc = { "Move split down" } })
+vim.keymap.set("n", "<leader>sK", "<C-w>K", { silent = true, noremap = true, desc = { "Move split up" } })
+vim.keymap.set("n", "<leader>sL", "<C-w>L", { silent = true, noremap = true, desc = { "Move split right" } })
+
+vim.keymap.set("n", "<leader>s=", "<C-w>=", { silent = true, noremap = true, desc = { "Equalize splits" } })
+vim.keymap.set("n", "<leader>s+", "<C-w>+", { silent = true, noremap = true, desc = { "Increase split height" } })
+vim.keymap.set("n", "<leader>s-", "<C-w>-", { silent = true, noremap = true, desc = { "Decrease split height" } })
+vim.keymap.set("n", "<leader>s>", "<C-w>>", { silent = true, noremap = true, desc = { "Increase split width" } })
+vim.keymap.set("n", "<leader>s<", "<C-w><", { silent = true, noremap = true, desc = { "Decrease split width" } })
+
+local n_opts = { silent = true, noremap = true, desc = { "Better window navigation" } }
+local t_opts = { silent = true, desc = { "Terminal mode window navigation" } }
 
 -- Better window navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h', n_opts)
